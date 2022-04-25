f6675a1dce73c82063ace9146c7ce6d122ee4f7c
Alex Ross <alxrg@me.com>
2022-04-24 21:49:02 -0500

    local storage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,37 @@ import { Provider } from 'react-redux';
 import reducers from './reducers';
 import PasswordList from './password-list';
 
-const store = createStore(reducers)
+const PSSWRD_STATE = "PASSWRD_STATE"
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem(PSSWRD_STATE)
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch(err) {
+    return undefined
+  }
+}
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem(PSSWRD_STATE, serializedState)
+  } catch(err) {
+    console.log("Error saving data:"+err)
+  }
+}
+
+// const store = createStore(reducers)
+
+const persistedState = loadState();
+const store = createStore(reducers, persistedState);
+store.subscribe(() => {
+  saveState(store.getState());
+})
+
 
 
 function App() {
